4932ece4cb1e01e0d421521714b684c4b3ab01de
Aditya Vardhan <adtyavrdhn@gmail.com>
2022-11-26 06:42:58 +0530

    water-animated

--- App.tsx
+++ App.tsx
@@ -18,43 +18,25 @@ const App = () => {
   const progress = useSharedValue(0);
   useEffect(() => {
     progress.value = withRepeat(
-      withTiming(1, {duration: 1000, easing: Easing.inOut(Easing.ease)}),
+      withTiming(1, {duration: 3000, easing: Easing.inOut(Easing.ease)}),
       -1,
       true,
     );
   }, [progress]);
-  const data1 = useDerivedValue(() => {
+  const data = useDerivedValue(() => {
     const m = mix.bind(null, progress.value);
     return {
       from: {
-        x: m(-0.1, -1),
-        y: m(0.2, 0.5),
+        x: m(-0.7, -1),
+        y: m(0.3, 0.5),
       },
-      c1: {x: m(0, 0.5), y: m(0.7, 1)},
+      c1: {x: m(0, 0.5), y: m(0.9, 1)},
       c2: {x: m(1, 0.5), y: m(0.3, 0)},
-      to: {x: m(1.1, 2), y: m(0.8, 0.5)},
-    };
-  });
-  const data2 = useDerivedValue(() => {
-    const m = mix.bind(null, 1 - progress.value);
-    return {
-      from: {
-        x: m(-0.1, -1),
-        y: m(0.2, 0.5),
-      },
-      c1: {x: m(0, 0.5), y: m(0.7, 1)},
-      c2: {x: m(1, 0.5), y: m(0.3, 0)},
-      to: {x: m(1.1, 2), y: m(0.8, 0.5)},
+      to: {x: m(1.4, 2), y: m(0.8, 0.6)},
     };
   });
   const path1 = useAnimatedProps(() => {
-    const {from, c1, c2, to} = data1.value;
-    return {
-      d: `M ${from.x} ${from.y} C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y} L 1 1 L 0 1 Z`,
-    };
-  });
-  const path2 = useAnimatedProps(() => {
-    const {from, c1, c2, to} = data2.value;
+    const {from, c1, c2, to} = data.value;
     return {
       d: `M ${from.x} ${from.y} C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y} L 1 1 L 0 1 Z`,
     };
@@ -77,7 +59,6 @@ const App = () => {
           />
         }>
         <Svg width={SIZE} height={SIZE} viewBox="0 0 1 1">
-          {/* <AnimatedPath fill="#86b4ff" animatedProps={path2} /> */}
           <AnimatedPath fill="#3884ff" animatedProps={path1} />
         </Svg>
       </MaskedView>
